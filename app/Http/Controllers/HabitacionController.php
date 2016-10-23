11dacd5e1e00bff3395e943dfc100f1477b7c7e6
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2016-10-23 01:10:58 -0300

    aceptar true o false en equipamiento de habitacion

--- app/Http/Controllers/HabitacionController.php
+++ app/Http/Controllers/HabitacionController.php
@@ -48,10 +48,10 @@ class HabitacionController extends Controller
 			'precio'		=> 'required|numeric',
 			'piso'			=> 'required|numeric',
 			'propiedad_id'  => 'required|numeric',
-			'bano'      	=> 'numeric',
-            'tv'        	=> 'numeric',
-            'wifi'      	=> 'numeric',
-            'frigobar'  	=> 'numeric',
+			'bano'      	=> 'required',
+            'tv'        	=> 'required',
+            'wifi'      	=> 'required',
+            'frigobar'  	=> 'required',
 
 			
 		);
@@ -84,10 +84,63 @@ class HabitacionController extends Controller
 
 
             $equipamiento                    	  	  = new Equipamiento();
-			$equipamiento ->bano              	 	  = $request->get('bano');
-			$equipamiento ->tv              		  = $request->get('tv');
-			$equipamiento ->wifi  	 			      = $request->get('wifi');
-			$equipamiento ->frigobar               	  = $request->get('frigobar');
+
+
+            if($request->get('bano') == 'true'){
+			$equipamiento ->bano              	 	  = '1';
+
+            }else{
+
+            if($request->get('bano') == 'false'){
+            $equipamiento ->bano                      = '0';
+
+            }
+            }
+
+            if($request->get('tv') == 'true'){
+
+			$equipamiento ->tv              		  = '1';
+            }else{
+
+            if($request->get('tv') == 'false'){
+
+            $equipamiento ->tv                        = '0';
+
+            }    
+
+
+            }
+
+            if($request->get('wifi') == 'true'){
+
+
+			$equipamiento ->wifi  	 			      = '1';
+
+            }else{
+
+
+            if($request->get('wifi') == 'false'){
+
+            $equipamiento ->wifi                      = '0';
+
+            }
+
+            }
+
+            if($request->get('frigobar') == 'true'){
+
+			$equipamiento ->frigobar               	  = '1';
+
+            }else{
+
+            if($request->get('frigobar') == 'false'){
+
+            $equipamiento ->frigobar                  = '0';
+
+
+            }
+
+            }
 
 			$equipamiento ->habitacion_id			  = $habitacion->id; 
 
