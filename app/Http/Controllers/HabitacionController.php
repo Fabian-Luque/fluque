5239d8a87f794c34d4ff31b787cbe06eab208cbf
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2016-10-24 18:49:13 -0300

    metodo update para actulizar habitacion

--- app/Http/Controllers/HabitacionController.php
+++ app/Http/Controllers/HabitacionController.php
@@ -110,7 +110,75 @@ class HabitacionController extends Controller
 
 
 
-	}
+	}  
+
+
+
+
+        public function update(Request $request, $id){
+
+
+        $rules = array(
+
+            'nombre'        => '',
+            'tipo'          => '',
+            'precio'        => 'numeric',
+            'piso'          => 'numeric',
+            'bano'          => '',
+            'tv'            => '',
+            'wifi'          => '',
+            'frigobar'      => '',
+            
+        );
+
+    $validator = Validator::make($request->all(), $rules);
+
+
+         if ($validator->fails()) {
+
+            $data = [
+
+                'errors' => true,
+                'msg' => $validator->messages(),
+
+            ];
+
+            return Response::json($data, 400);
+
+        } else {
+
+            $habitacion = Habitacion::findOrFail($id);
+
+            $habitacion->update($request->all());
+            $habitacion->touch();
+            
+
+
+            $equipamiento = Equipamiento::findOrFail($id);
+
+            $equipamiento->update($request->all());
+            $equipamiento->touch();
+
+            $data = [
+
+                'errors' => false,
+                'msg' => 'Habitacion actualizada satisfactoriamente',
+
+            ];
+
+            return Response::json($data, 201);
+
+        }
+
+
+
+
+
+
+
+
+    }
+
 
 
 
