525843f80e91b39a1c7145e0829655141e602b6a
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-01-30 14:57:44 -0300

    modificar columnas tabla huesedes

---
+++ database/migrations/2017_01_30_170749_change_attributes_huespedes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeAttributesHuespedesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('huespedes', function(Blueprint $table){
+        $table->string('email')->nullable()->change();
+        $table->integer('telefono')->nullable()->change();
+        $table->string('pais')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
