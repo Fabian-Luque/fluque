5838d7b22fc01b12547c64628192f3d9489a243c
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-02-10 23:45:27 -0300

    actualiza columna tipo de tabla pagos

---
+++ database/migrations/2017_12_22_023549_change_column_tipo_pagos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnTipoPagosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE pagos MODIFY tipo ENUM('Confirmacion de pago','Pago parcial o total','Pago consumos')");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
