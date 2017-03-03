e260ab860752a0bb897b2d74390f802ded0108fa
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-03-02 23:05:37 -0300

    data seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       //$this->call(TipoHabitacionTableSeeder::class);
-       //$this->call(TipoPropiedadTableSeeder::class);
-       //$this->call(TipoFuenteTableSeeder::class);
-       //$this->call(MetodoPagoTableSeeder::class);
-       //$this->call(EstadoReservaTableSeeder::class);
-       //$this->call(TipoClienteTableSeeder::class);
-       //$this->call(TipoComprobanteTableSeeder::class);
+       $this->call(TipoHabitacionTableSeeder::class);
+       $this->call(TipoPropiedadTableSeeder::class);
+       $this->call(TipoFuenteTableSeeder::class);
+       $this->call(MetodoPagoTableSeeder::class);
+       $this->call(EstadoReservaTableSeeder::class);
+       $this->call(TipoClienteTableSeeder::class);
+       $this->call(TipoComprobanteTableSeeder::class);
        $this->call(CategoriasTableSeeder::class);
     }
 }
