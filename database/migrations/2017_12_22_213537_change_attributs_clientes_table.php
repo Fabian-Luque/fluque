f6a34dd6b1b8c0c363ef593961bc0f2dcd0f7522
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-03-04 01:18:05 -0300

    cambia atributos email y rut de clientes los deja nulos

--- database/migrations/2017_12_22_213537_change_attributs_clientes_table.php
+++ database/migrations/2017_12_22_213537_change_attributs_clientes_table.php
@@ -14,6 +14,7 @@ class ChangeAttributsClientesTable extends Migration
     {
         Schema::table('clientes', function(Blueprint $table){
             $table->string('rut')->nullable()->change();
+            $table->string('email')->nullable()->change();
         });
     }
 
