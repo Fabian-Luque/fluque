a4eb8bf67ecc2dd9be9d171d3aaef5607fa8fc17
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-03-20 18:39:51 -0300

    agrega tipomoneda a databasesedder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
        $this->call(TipoClienteTableSeeder::class);
        $this->call(TipoComprobanteTableSeeder::class);
        $this->call(CategoriasTableSeeder::class);
+       $this->call(TipoMonedaTableSeeder::class);
     }
 }
