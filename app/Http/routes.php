dd3156eacaa2e0f825f8f0ed2486c4bfb34d0ee0
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-07-10 16:31:08 -0300

    pdf de reportes de pagos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -49,6 +49,7 @@ Route::group(['as' => 'api.jarvis.'], function(){
 	Route::post('pdf/reporte', 'PDFController@reporte');
 	Route::post('pdf/huesped', 'PDFController@huesped');
 	Route::post('pdf/checkin', 'PDFController@checkin');
+	Route::post('pdf/pagos', 'PDFController@pagos');
 	Route::post('ingreso/servicio', 'PropiedadController@ingresoServicio');
 	Route::post('ingreso/servicio/cliente', 'ClienteController@ingresoServicio');
 	Route::get('cliente/empresa', 'ClienteController@getClientes');
