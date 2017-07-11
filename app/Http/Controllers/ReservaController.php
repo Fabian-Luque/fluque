082685e6e4e853afea1c3e824347daaff4356c59
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-07-10 22:00:42 -0300

    cambio de estado de reserva al eliminar pago de reserva finalizada

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -101,6 +101,9 @@ class ReservaController extends Controller
       }
     }
     $pago->delete();
+    if ($reserva->estado_reserva_id == 4) {
+       $reserva->update(array('estado_reserva_id' => 5));
+    }
 
     $retorno = [
         'errors' => false,
