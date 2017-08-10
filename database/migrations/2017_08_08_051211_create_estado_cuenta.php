95708836a65094128482ac32af3c398556764f95
Daniel Heresmann <daniel@holajarvis.com>
2017-08-10 17:20:28 -0400

    correccion nombre migracion

--- database/migrations/2017_08_08_051211_create_estado_cuenta.php
+++ database/migrations/2017_08_08_051211_create_estado_cuenta.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EstadoCuenta extends Migration {
+class CreateEstadoCuenta extends Migration {
     /**
      * Run the migrations.
      *
