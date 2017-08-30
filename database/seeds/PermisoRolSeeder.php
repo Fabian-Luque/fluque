470664c06b2f4bc7ea58f4507729ef54c6c4f69c
Allen Rodriguez Yunnissi <Allen Rodriguez Yunnissi>
2017-08-30 20:14:40 -0300

    arregla seeder permiso rol

--- database/seeds/PermisoRolSeeder.php
+++ database/seeds/PermisoRolSeeder.php
@@ -14,169 +14,169 @@ class PermisoRolSeeder extends Seeder
       	DB::table('permiso_rol')->insert([
     		[
     			'permiso_id' => 1,
-    			'rol'        => 1,
+    			'rol_id'     => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 2,
-    			'rol'        => 1,
+    			'rol_id'     => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 3,
-    			'rol'        => 1,
+    			'rol_id'     => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 4,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 5,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 6,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 7,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 8,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 9,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 10,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 11,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 12,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 13,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 14,
-    			'rol'        => 1,
+    			'rol_id'     => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 15,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 16,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 17,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 18,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 19,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 20,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 21,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 22,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 23,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 24,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 25,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 26,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 27,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		],
     		[
     			'permiso_id' => 28,
-    			'rol'        => 1,
+    			'rol_id'        => 1,
     			'estado'     => 1,
 
     		]
