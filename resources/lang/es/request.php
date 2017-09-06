4e3e109148f0d62416e9d0e4930dc1a50901dd24
Daniel Heresmann <daniel@holajarvis.com>
2017-09-06 10:39:20 -0300

    status code

--- resources/lang/es/request.php
+++ resources/lang/es/request.php
@@ -3,14 +3,14 @@
 return [
     'success' => [
         'status' => 'false',
-        'code'   => 200,
+        'code'   => '200',
     ],
     'failure' => [
         'status' => 'true',
         'code'  => [
-            'not_founded' => 404, // no encontrado
-            'bad_request' => 400, // peticion mala
-            'forbidden'   => 403, // prohibido
+            'not_founded' => '404', // no encontrado
+            'bad_request' => '400', // peticion mala
+            'forbidden'   => '403', // prohibido
         ],
         'bad'    => 'Solicitud incorrecta.',
     ],
