256fd53cac6305de1a33c5144b167c25e0ba2efd
Allen <allenrcs@gmail.com>
2017-09-19 13:43:40 -0300

    accesor fecha apertura cajas

--- app/Caja.php
+++ app/Caja.php
@@ -42,6 +42,16 @@ class Caja extends Model
         ->withTimestamps();
     }
 
+    public function getFechaAperturaAtAttribute($value)
+    {
+        $user            = JWTAuth::parseToken()->toUser();
+        $propiedad       = $user->propiedad[0];
+        $zona_horaria_id = $propiedad->zona_horaria_id;
+        $zona_horaria    = ZonaHoraria::where('id', $zona_horaria_id)->first();
+        $pais            = $zona_horaria['nombre'];
+        return Carbon::parse($value)->timezone($pais)->format('Y-m-d H:i:s');
+    }   
+
     public function getCreatedAtAttribute($value)
     {
         $user            = JWTAuth::parseToken()->toUser();
