ecc6e449f6edfb5271d17428b23000a3f4f99ad9
Allen <allenrcs@gmail.com>
2017-09-19 13:03:44 -0300

    agrega accesor para cambio de hora de fecha apertura de cajas

--- app/Caja.php
+++ app/Caja.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\ZonaHoraria;
+use JWTAuth;
+use \Carbon\Carbon;
 
 class Caja extends Model
 {
@@ -38,6 +41,16 @@ class Caja extends Model
         ->withPivot('id', 'monto', 'descripcion', 'tipo_moneda_id')
         ->withTimestamps();
     }
+
+    public function getFechaAperturaAtAttribute($value)
+    {
+        $user            = JWTAuth::parseToken()->toUser();
+        $propiedad       = $user->propiedad[0];
+        $zona_horaria_id = $propiedad->zona_horaria_id;
+        $zona_horaria    = ZonaHoraria::where('id', $zona_horaria_id)->first();
+        $pais            = $zona_horaria['nombre'];
+        return Carbon::parse($value)->timezone($pais)->format('Y-m-d H:i:s');
+    }   
 
 
 
