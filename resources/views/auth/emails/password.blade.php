70e040d0637da3e8dc5f1f6e2a0a2111f78cbcb2
Daniel Heresmann <daniel@holajarvis.com>
2017-10-15 18:48:59 -0300

    diseño recuperar contraseña

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,54 +1,40 @@
-Haga click aqui para restablecer : 
-<a href="{{ $link = url('reset/password', $request->token).'?email='.urlencode($request->destino) }}"> 
-{{ $link }} 
-</a>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <style type="text/css">
+          body {
+            margin: 0; 
+ 
+            min-width: 600px!important;
+          }
+          .content {
+            width: 100%; 
+            max-width: 600px;
+          }  
+        </style>
 
+    </head>
+    <body bgcolor="#f7f7f7">
+        <table style="padding-top: 5px;" width="100%" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0">
+            <tr style="padding-top: 5px; padding-bottom: 5px;">
+              <img src="https://www.gofeels.com/wp-content/uploads/2017/07/logodark.png" width="110" height="40" border="0">
 
-
-
-
-<div id=":10a" class="ii gt adP adO">
-  <div id=":11z" class="a3s aXjCH m15f1560fa32f7cc1"><u></u>
-
-
-
-<div bgcolor="#ececec" style="padding:0px;margin:0px">
-<table width="100%" style="table-layout:fixed;margin:0 auto" border="0" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td align="center" class="m_3359992204289625023bgnone" bgcolor="#ececec">
-
-<table width="640" class="m_3359992204289625023w320" border="0" cellspacing="0" cellpadding="0">
-
-<tbody>
-<tr>
-<td width="640">
-
-
-
-
-
-<table width="700" class="m_3359992204289625023w320" cellpadding="0" cellspacing="0" border="0" bgcolor="#f7f7f7" style="width:640px; padding-top: 25px; padding-bottom: 15px;">
-  <tbody><tr>
-  <td bgcolor="#f7f7f7">
-<a href="http://news.neteller.com/pub/cc?_ri_=X0Gzc2X%3DYQpglLjHJlTQGs2zgAUngU5ULTuLFWhW50zazdel0zftabOqTCUqEfL8zghs3hEL1VXtpKX%3DYUWTT&amp;_ei_=EolaGGF4SNMvxFF7KucKuWNzXOJKdfFZQjUgjtOKnnuYAMCPQ37PxH4m5vr3SaL7u1VGrAKoyc2573HXtCjLSuQMuWUJ90AN_XEZyYV-2UJSyic." target="_blank" data-saferedirecturl="https://www.google.com/url?hl=es&amp;q=http://news.neteller.com/pub/cc?_ri_%3DX0Gzc2X%253DYQpglLjHJlTQGs2zgAUngU5ULTuLFWhW50zazdel0zftabOqTCUqEfL8zghs3hEL1VXtpKX%253DYUWTT%26_ei_%3DEolaGGF4SNMvxFF7KucKuWNzXOJKdfFZQjUgjtOKnnuYAMCPQ37PxH4m5vr3SaL7u1VGrAKoyc2573HXtCjLSuQMuWUJ90AN_XEZyYV-2UJSyic.&amp;source=gmail&amp;ust=1507998252454000&amp;usg=AFQjCNFYU-bVVdg454JHntE-DDg21Hyx9Q">
-
-<img src="https://www.gofeels.com/wp-content/uploads/2017/07/logodark.png" width="110" height="40" alt="NETELLER" border="0" class="CToWUd"></a>
-</td>
-  </tr>  
-</tbody></table>
-
-
-
-<table width="640" class="m_3359992204289625023w320" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" style="width:640px padding-bottom:20px;">
-<tbody><tr>
-<td>
-</td>
-</tr>
-</tbody></table>
-<table width="640" class="m_3359992204289625023w320" cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff" style="width:640px">
-  <tbody><tr>
-    <td style="font-family:Arial;font-size:14px;line-height:19px;color:#544c64; padding-left: 60px; padding-right: 60px;padding-bottom: 20px; padding-top: 25px;">
+            </tr>
+            <div style="color: #fcfcfc;">
+                          <tr>
+              <td>
+           <td style="
+              font-family:Arial;
+              font-size:14px;
+              line-height:19px;
+              color:#544c64; 
+              padding-left: 60px; 
+              padding-right: 60px;
+              padding-bottom: 20px; 
+              padding-top: 25px;
+              text-align: center;
+            ">
 
     <h2 style="color: #006cbf;">
     <strong>
@@ -56,61 +42,145 @@ Haga click aqui para restablecer :
     </strong>
     </h2>
 
-      <div style="color: #000000; padding-right: 35px; ">
+      <div style=" font-size: 18px; color: #000000; padding-right: 35px; text-align: center; letter-spacing: normal">
         <strong>
           Tú nos dijiste que acabas de perder tu contraseña sí tu realmente lo hiciste, haz click en el siguiente boton para cambiar la contraseña.
         </strong>
       </div>
 
-          <br>
+          <br><br>
 
 
 
-  </tr>
-</tbody></table>
+
+
+    <a style="
+    text-decoration: none; 
+    height: 50px;
+    background-color: #006cbf;
+    text-align: center; 
+    font-size: 20px; 
+    font-family: sans-serif; 
+    font-weight: bold; 
+    padding: 0 40px 0 40px; 
+    color: #ffffff; 
+    padding-top: 10px; 
+    padding-bottom: 10px;
+    border: 5px outset #006cbf;
+    " href="https://www.facebook.com/"> Crea una nueva contraseña</a>
+
+    <br><br><br>
+
+    <div style="font-size: 18px; color: #000000; padding-right: 35px; text-align: center; letter-spacing: normal">
+      
+          Tú nos dijiste que acabas de perder tu contraseña sí tu realmente lo hiciste, haz click en el siguiente boton para cambiar la contraseña.
+      
+      </div>
+
+  </td>
+              </td>
+
+            </tr>
+            </div>
+        </table>
+        <footer>
+          <center>
+          <hr style="
+            height: 5px;
+          background-color: #ffaa00;
+        ">
+            Desarrollado por HolaJarvis Spa
+          </center>
+        </footer>
+    </body>
+</html>
 
 
 
-<table width="640" class="m_3359992204289625023w320" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" style="width:640px">
-<tbody><tr>
-<td><img src="https://ci4.googleusercontent.com/proxy/v2LIk5A1-T7TJsIj6oX_m-CaagrTH4zqpQtph2PIdQlZpHao4d3GCrw8zLkuVTKQdj1Wf0bDMktn-VGrWYCrWsCXaS7ukPLfNGo23g9SJnc=s0-d-e1-ft#http://storage.neteller.com/email/master_images/spacer.gif" alt="" width="640" height="23" bgcolor="#FFFFFF" border="0" class="m_3359992204289625023w100pc CToWUd">
-</td>
-</tr>
-</tbody></table>
 
 
 
-<table width="640" border="0" cellspacing="0" cellpadding="0" class="m_3359992204289625023w320" style="width:640px">
-  <tbody><tr>
-    <td colspan="3"><img src="https://ci4.googleusercontent.com/proxy/v2LIk5A1-T7TJsIj6oX_m-CaagrTH4zqpQtph2PIdQlZpHao4d3GCrw8zLkuVTKQdj1Wf0bDMktn-VGrWYCrWsCXaS7ukPLfNGo23g9SJnc=s0-d-e1-ft#http://storage.neteller.com/email/master_images/spacer.gif" width="1" height="20" style="display:block" class="CToWUd"></td>
-  </tr>
-  <tr>
-    <td width="30" class="m_3359992204289625023w20" style="font-size:1px;line-height:1px"><img src="https://ci4.googleusercontent.com/proxy/v2LIk5A1-T7TJsIj6oX_m-CaagrTH4zqpQtph2PIdQlZpHao4d3GCrw8zLkuVTKQdj1Wf0bDMktn-VGrWYCrWsCXaS7ukPLfNGo23g9SJnc=s0-d-e1-ft#http://storage.neteller.com/email/master_images/spacer.gif" width="30" class="m_3359992204289625023w20 CToWUd" height="1" style="display:block"></td>
-
-    <td width="30" class="m_3359992204289625023w20" style="font-size:1px;line-height:1px"><img src="https://ci4.googleusercontent.com/proxy/v2LIk5A1-T7TJsIj6oX_m-CaagrTH4zqpQtph2PIdQlZpHao4d3GCrw8zLkuVTKQdj1Wf0bDMktn-VGrWYCrWsCXaS7ukPLfNGo23g9SJnc=s0-d-e1-ft#http://storage.neteller.com/email/master_images/spacer.gif" width="30" class="m_3359992204289625023w20 CToWUd" height="1" style="display:block"></td>
-  </tr>
-</tbody></table>
-
-
-  
-<div style="clear:both"></div>
-
-
- 
-
-
-</td>
-
-</tr>
-</tbody></table>
-</td>
-</tr>
-</tbody>
-</table>
-<u></u>
 
 
 
-    </div>
-  </div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
