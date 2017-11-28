0585985f7ca44d036b58f1be76bd370386f4db61
Allen <allenrcs@gmail.com>
2017-11-28 01:12:56 -0300

    cambia motorcontroler widget

--- app/Http/Controllers/MotorWidgetControllers/MotorController.php
+++ app/Http/Controllers/MotorWidgetControllers/MotorController.php
@@ -1,13 +1,19 @@
 <?php
+
 namespace App\Http\Controllers\MotorWidgetControllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Response;
+
 class MotorController extends Controller {
     public function getMotor(Request $request) {
         return '
         <style media="screen">
+            .md-button.md-icon-button {
+                background: none !important;
+            }
             .contenedor-motor-reserva {
                 height: 90px;
                 background-color: transparent;
@@ -18,19 +24,20 @@ class MotorController extends Controller {
             .contenedor-motor-reserva .mdr-btn {
                 font-size: 13px;
             }
-            .contenedor-motor-reserva .mdr-gf-datepicker {
-                .main-content button { 
-                    background: none !important;
-                }
-            }
         </style>
+
+
         <div class="contenedor-motor-reserva" layout="row" layout-align="center center" md-theme="mdrTheme" ng-cloak>
             <form name="motorReservaForm">
                 <md-datepicker class="mdr-gf-datepicker" name="checkin" ng-model="check_in" md-placeholder="Check in" md-min-date="minDate" ng-change="cambioCheckIn()" required style="width:100px;"></md-datepicker>
                 <md-datepicker class="mdr-gf-datepicker" name="checkout" ng-model="check_out" md-placeholder="Check out" md-min-date="fecha_limite_2" required></md-datepicker>
             </form>
+
             <md-button class="md-raised md-accent mdr-btn" ng-click="buscarDisponibilidad(motorReservaForm);" ui-sref="asd" ng-disabled="motorReservaForm.$invalid">buscar</md-button>
+
         </div>
 ';
     }
+
+
 }
