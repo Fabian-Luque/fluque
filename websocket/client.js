2c32d60b9d48dbdaffe67d2639b1b6ba9655bc61
Daniel Heresmann <daniel@holajarvis.com>
2017-11-29 01:24:02 -0300

    fix rama

--- websocket/client.js
+++
@@ -1,7 +0,0 @@
-const io = require('socket.io')();
-
-var socket = io.connect('http://localhost:3000');
-socket.on('message', function (data) {
-    //Just console the message and user right now.
-    console.log(data.message+" " + data.user);
-});
--- websocket/client.py
+++
@@ -1,7 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://localhost:3000")
-print "Sending 'Hello, World'..."
-
-ws.send("holaaaa")
-print "enviado!!"
-ws.close()
