f95028617ee1af2dcb095e4ae19ad9a9f84dd2d1
Allen <allenrcs@gmail.com>
2017-12-07 13:20:30 -0300

    arregla pdf pagos

--- resources/views/pdf/pagos.blade.php
+++ resources/views/pdf/pagos.blade.php
@@ -28,45 +28,7 @@
 
         <div class="contenedor">
           <div class="estado-cuenta">
-
-
-
-
-                    <!-- <div class="titulo" style="text-align:left;">
-                      <h3 class="">{{ $fecha_inicio->format('d/m/Y') }} - {{ $fecha_fin->format('d/m/Y') }}</h3>
-                    </div>
-
-                    <div class="detalles" style="margin:20px 0px;">
-                      <table class="tabla-detalles">
-                      	
-                        <tr>
-                          <th class="data-tabla-detalles borde-derecha"><p>Fecha</p></th>
-                @foreach($propiedad as $prop)
-                	@foreach($prop['tipoMonedas'] as $moneda)
-                          <th class="data-tabla-detalles borde-derecha"><p>{{ $moneda->nombre }}</p></th>
-                    @endforeach
-                @endforeach
-                        </tr>
-        		    @foreach($fechas as $fecha)
-                        <tr style="text-align:center;">
-                          <td class="data-tabla-detalles borde-derecha"><p>{{ $fecha['fecha'] }}</p></td>
-        			  @foreach($fecha['moneda'] as $moneda)
-                          <td class="data-tabla-detalles borde-derecha"><p>{{ $moneda['nombre'] }} ${{ $moneda['suma'] }}</p></td>
-                    @endforeach
-                        </tr>
-        		    @endforeach
-                      
-                        <tr style="text-align:center;">
-                          <td class="data-tabla-detalles borde-derecha"><p>Total</p></td>
-                    @foreach($ingresos_totales as $total)
-                          <td class="data-tabla-detalles borde-derecha"><p>{{ $total['nombre_moneda'] }} ${{ $total['monto'] }}</p></td>
-                    @endforeach
-                        </tr>
-
-                      </table>
-                    </div> -->
-
-
+          
                     <div class="detalles" style="margin:20px 0px;">
                         
 
@@ -96,15 +58,19 @@
                           <th class="data-tabla-detalles borde-derecha"><p>Monto</p></th>
                         </tr>
 
-
-
-
                         @foreach($fecha['ps'] as $pago)
                         <tr>
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->numero_reserva }} </p></td>
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->tipo }} </p></td>
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->nombre_metodo_pago }},  {{ $pago->numero_cheque }} </p></td>
+                          @if($pago->tipoComprobante != null)
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->tipoComprobante->nombre }} </p></td>
+
+                          @else
+                          <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> </p></td>
+
+
+                          @endif
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->numero_operacion }} </p></td>
                           <td class="data-tabla-detalles borde-derecha" style="text-align:center;"><p> {{ $pago->nombre_tipo_moneda }} ${{ $pago->monto_equivalente }} </p></td>
                         </tr>
@@ -120,20 +86,6 @@
 
 
                     </div>
-
-
-
-
-                   
-                    
-
-
-                    
-
-
-
-
-
 
           </div>
           <!--  Fin estado-cuenta  -->
