edc78cd56d0220670bd2b7cbb084f8c55c1191f4
Daniel Heresmann <daniel@holajarvis.com>
2017-12-07 23:27:00 -0300

    correccion

--- app/Http/Controllers/DashControllers/UserDashController.php
+++ app/Http/Controllers/DashControllers/UserDashController.php
@@ -57,8 +57,118 @@ class UserDashController extends Controller {
                 );
                 $ubicacion->save();
 
-                $job = new ProcesoQVO($usuario);
-                dispatch($job);
+                $user = $usuario;
+        $client = new Client();
+
+        \Log::info($user);
+        if (!is_null($user)) {
+            \Log::info("si es nulo\n");
+            try {
+                \Log::info("peticion http\n");
+                $body = $client->request(
+                    'POST',  
+                    config('app.qvo_url_base').'/customers', [
+                        'json' => [
+                            'email' => $user->email,
+                            'name'  => $user->propiedad[0]->nombre
+                        ],
+                        'headers' => [
+                            'Authorization' => 'Bearer '.config('app.qvo_key')
+                        ]
+                    ]
+                )->getBody();
+    
+
+                $response = json_decode($body);
+                $retorno["msj"] = $response;
+
+                $qvo_user = new QvoUser();
+                $qvo_user->prop_id = $user->propiedad[0]->id;
+                $qvo_user->qvo_id  = $response->id;
+                
+                if ($qvo_user->save()) {
+                    try {
+                        $body = $client->request(
+                            'POST', 
+                            config('app.DOLAR_PRICE_API')
+                        )->getBody();
+            
+                        $dolar_price = json_decode($body); 
+                        $dolar_price = intval($dolar_price->quotes->USDCLP);
+                    } catch (GuzzleException $e) {
+                        $dolar_price = 600;
+                    }
+
+                    if ($user->propiedad[0]->numero_habitaciones > 27) {
+                        $precio = round($dolar_price * 27);    
+                    } else {
+                        $precio = round(
+                            $dolar_price * $user->propiedad[0]->numero_habitaciones
+                        );
+                    }
+
+                    try {
+                        $body = $client->request(
+                            'POST',  
+                            config('app.qvo_url_base').'/plans', [
+                                'json' => [
+                                    'id' => $user->propiedad[0]->id,
+                                    'name' => $user->propiedad[0]->nombre,
+                                    'price' => $precio * intval(
+                                        config('app.PRECIO_X_HAB_QVO')
+                                    ),
+                                    'currency' => 'CLP',
+                                    'interval' => 'year',
+                                    'trial_period_days' => 15
+                                ],
+                                'headers' => [
+                                    'Authorization' => 'Bearer '.config('app.qvo_key')
+                                ]
+                            ]
+                        )->getBody();
+
+                        $response = json_decode($body);
+
+                        try {
+                                $body = $client->request(
+                                    'POST', 
+                                    config('app.qvo_url_base').'/subscriptions', [
+                                            'json' => [
+                                                'customer_id' => $qvo_user->qvo_id,
+                                                'plan_id' => $user->propiedad[0]->id
+                                            ],
+                                            'headers' => [
+                                                'Authorization' => 'Bearer '.config('app.qvo_key')
+                                            ]
+                                        ]
+                                )->getBody();
+                                $response = json_decode($body);
+
+                                $qvo_user->solsub_id = $response->id;
+                                $qvo_user->save(); 
+
+                                $retorno["msj"]    = $response;
+                            } catch (GuzzleException $e) {
+                                $retorno["msj"]    = json_decode((string)$e->getResponse()->getBody());
+                            } 
+                    } catch (GuzzleException $e) {
+                        $retorno["msj"] = json_decode(
+                            (string)$e->getResponse()->getBody()
+                        );
+                    } 
+                } else {
+                    \Log::error('Error al crear el cliente');
+                }
+            } catch (GuzzleException $e) {
+                $retorno["msj"] = json_decode(
+                    (string)$e->getResponse()->getBody()
+                );
+            } 
+        } else {
+            $status            = trans('request.failure.code.bad_request');
+            $retorno['errors'] = true;
+            $retorno['msj']    = "El usuario no se encuentra registrado";
+        }
                 
                 $data['accion'] = 'Crear usuario';
                 $data['msg'] = 'Usuario creado satisfactoriamente';
