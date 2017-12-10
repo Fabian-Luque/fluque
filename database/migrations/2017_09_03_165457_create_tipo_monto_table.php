564ad08690a4309dfa8a049f3d28ddedd26e4f0b
Daniel Heresmann <daniel@holajarvis.com>
2017-12-10 15:54:39 -0300

    correccion error migraciones

--- database/migrations/2017_09_03_165457_create_tipo_monto_table.php
+++ database/migrations/2017_09_03_165457_create_tipo_monto_table.php
@@ -5,18 +5,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTipoMontoTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+ 
+
     public function up()
     {
+        if (!Schema::hasTable('tipo_monto')) {
         Schema::create('tipo_monto', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
             $table->timestamps();
         });
+    }
     }
 
     /**
