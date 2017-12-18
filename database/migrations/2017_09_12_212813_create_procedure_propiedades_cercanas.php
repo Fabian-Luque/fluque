5870ffe5ebeb71850d982f28b2daa8092e090017
Daniel Heresmann <daniel@holajarvis.com>
2017-12-18 16:25:52 -0300

    fix migration ub prop

---
+++ database/migrations/2017_09_12_212813_create_procedure_propiedades_cercanas.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProcedurePropiedadesCercanas extends Migration {
+    /**
+     * Procedimiento almacenado que retorna las propiedades cercanas
+        a partir de un punto(latitud , longitud) y en un radio determinado 
+        en kilometros cuadrados
+     *
+     * @return void
+     */
+    public function up() {
+       $sql = "
+CREATE PROCEDURE propiedades_cercanas(pos_lat DOUBLE, pos_lng DOUBLE, radio_km DOUBLE)
+BEGIN
+    DECLARE fin INT;
+    DECLARE idd INT DEFAULT 0;
+    DECLARE prop_idd INT DEFAULT 0;
+    DECLARE latitudd DOUBLE DEFAULT 0;
+    DECLARE longitudd DOUBLE DEFAULT 0;
+
+    DECLARE cur1 CURSOR
+    FOR
+    SELECT 
+        id,
+        prop_id,
+        X(`location`) as latitud,
+        Y(`location`) as longitud
+    FROM `ubicacion_propiedad`
+    WHERE ( 
+        6372.795477598 *
+        ACOS(
+            SIN ( RADIANS(x(location)) ) * SIN( RADIANS((pos_lat)) ) + 
+            COS ( RADIANS(x(location)) ) * COS( RADIANS((pos_lat)) ) * 
+            COS ( RADIANS(y(location)) - RADIANS(pos_lng) )
+        )
+    ) < radio_km;
+
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET fin=1;    
+
+    OPEN cur1;
+        ubicaciones: LOOP
+        FETCH cur1 INTO idd, prop_idd, latitudd, longitudd;
+ 
+        IF fin = 1 THEN 
+            LEAVE ubicaciones;
+        END IF;
+
+        SELECT idd as id, prop_idd as prop_id, latitudd as latitud, longitudd as longitud;
+ 
+    END LOOP ubicaciones;
+    CLOSE cur1;
+END"; 
+        //DB::connection()->getPdo()->exec($sql); 
+        DB::unprepared($sql);
+    }
+
+    public function down(){
+        $sql = "DROP PROCEDURE IF EXISTS propiedades_cercanas";
+        DB::unprepared($sql);
+    }
+}
