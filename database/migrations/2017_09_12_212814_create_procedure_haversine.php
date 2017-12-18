3517d4f616dd445b6876c2abc4b2ecc29d550434
Daniel Heresmann <daniel@holajarvis.com>
2017-12-18 18:59:49 -0300

    cambio procedure haversine

--- database/migrations/2017_09_12_212814_create_procedure_haversine.php
+++ database/migrations/2017_09_12_212814_create_procedure_haversine.php
@@ -5,20 +5,22 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProcedureHaversine extends Migration {
     public function up() {
+        $sql = "DROP PROCEDURE IF EXISTS haversine";
+        DB::unprepared($sql);
         $sql = "
-            CREATE PROCEDURE haversine(pos_lat DOUBLE, pos_lng DOUBLE)
-            RETURNS FLOAT
-            NO SQL DETERMINISTIC
-            BEGIN
-                RETURN (
-                    6372.795477598 *
-                    ACOS(
-                        SIN ( RADIANS(x(location)) ) * SIN( RADIANS((pos_lat)) ) + 
-                        COS ( RADIANS(x(location)) ) * COS( RADIANS((pos_lat)) ) * 
-                        COS ( RADIANS(y(location)) - RADIANS(pos_lng) )
-                    )
-                );
-            END
+            CREATE FUNCTION `haversine`(location GEOMETRY, pos_lat DOUBLE, pos_lng DOUBLE) RETURNS DOUBLE
+                NO SQL
+                DETERMINISTIC
+                BEGIN
+                    RETURN (
+                        6372.795477598 *
+                        ACOS(
+                            SIN ( RADIANS(x(location)) ) * SIN( RADIANS((pos_lat)) ) + 
+                            COS ( RADIANS(x(location)) ) * COS( RADIANS((pos_lat)) ) * 
+                            COS ( RADIANS(y(location)) - RADIANS(pos_lng) )
+                        )
+                    );
+                END
         ";
         DB::unprepared($sql);
     }
