259533e4097e758ee3393a9654802a18b96b411f
Daniel Heresmann <daniel@holajarvis.com>
2018-01-05 13:44:40 -0300

    ruta de prueba

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use App\Propiedad;
+use PDF;
 
 class Controller extends BaseController
 {
