46632b2c700e2fb446d4b6e5bbf8bdb53a608b0e
Daniel Heresmann <daniel@holajarvis.com>
2018-01-05 13:14:52 -0300

    mover funcion envio de correo a controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,48 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function EnvioCorreo(Propiedad $propiedad, $cliente_email, $arr, $vista_coreo, $vista_pdf, $nombre_pdf, $opcion, $propiedad_email) {
+
+        if ($opcion == 0) { // solo descarga
+            $pdf = PDF::loadView(
+                $vista_pdf, 
+                $arr
+            );
+
+            return $pdf;
+        } elseif ($opcion == 1) { // solo envio de correo
+            $job = new SendMail(
+                $propiedad,
+                $cliente_email,
+                $propiedad_email,
+                $vista_coreo,
+                $vista_pdf,
+                $nombre_pdf,
+                $arr
+            );
+
+            $this->dispatch($job);
+            return;
+        } else { // ambos!!!!!!!!!!!!!!!!!!!!
+            $job = new SendMail(
+                $propiedad,
+                $cliente_email,
+                $propiedad_email,
+                $vista_coreo,
+                $vista_pdf,
+                $nombre_pdf,
+                $arr
+            );
+
+            $this->dispatch($job);
+
+            $pdf = PDF::loadView(
+                $vista_pdf, 
+                $arr
+            );
+
+            return $pdf;
+        } 
+    }
 }
--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -1048,50 +1048,6 @@ class PDFController extends Controller
 
     }
 
-    public function EnvioCorreo(Propiedad $propiedad, $cliente_email, $arr, $vista_coreo, $vista_pdf, $nombre_pdf, $opcion, $propiedad_email) {
-
-        if ($opcion == 0) { // solo descarga
-            $pdf = PDF::loadView(
-                $vista_pdf, 
-                $arr
-            );
-
-            return $pdf;
-        } elseif ($opcion == 1) { // solo envio de correo
-            $job = new SendMail(
-                $propiedad,
-                $cliente_email,
-                $propiedad_email,
-                $vista_coreo,
-                $vista_pdf,
-                $nombre_pdf,
-                $arr
-            );
-
-            $this->dispatch($job);
-            return;
-        } else { // ambos!!!!!!!!!!!!!!!!!!!!
-            $job = new SendMail(
-                $propiedad,
-                $cliente_email,
-                $propiedad_email,
-                $vista_coreo,
-                $vista_pdf,
-                $nombre_pdf,
-                $arr
-            );
-
-            $this->dispatch($job);
-
-            $pdf = PDF::loadView(
-                $vista_pdf, 
-                $arr
-            );
-
-            return $pdf;
-        } 
-    }
-
     public function comprobanteReservaResumen(Request $request) {
         $reservas     = $request['reservas'];
         $propiedad_id = $request->input('propiedad_id');
