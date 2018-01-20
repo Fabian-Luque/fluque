5bed8f5fd87d01edc4e398f5cded93a8e816c8ec
Allen <allenrcs@gmail.com>
2018-01-20 19:58:06 -0300

    fix hora de  mensajes del chat

--- app/Mensajeria.php
+++ app/Mensajeria.php
@@ -14,4 +14,19 @@ class Mensajeria extends Model {
 		'mensaje', 
 		'created_at'
 	];
+
+	public function getCreatedAtAttribute($value)
+    {
+        $token = JWTAuth::getToken();
+        if ($token) {
+    	$user            = JWTAuth::parseToken()->toUser();
+        $propiedad       = $user->propiedad[0];
+        $zona_horaria_id = $propiedad->zona_horaria_id;
+        $zona_horaria    = ZonaHoraria::where('id', $zona_horaria_id)->first();
+        $pais            = $zona_horaria['nombre'];
+        return Carbon::parse($value)->timezone($pais)->format('Y-m-d H:i:s');
+        } 
+    } 
+
+	
 }
