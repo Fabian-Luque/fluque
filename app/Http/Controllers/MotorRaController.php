88c4aa565d173c61d17f30e989e7e14fc0297ad1
dheresmann <daniel@holajarvis.com>
2018-01-22 11:43:03 -0300

    mostrar linea de error en correo

--- app/Http/Controllers/MotorRaController.php
+++ app/Http/Controllers/MotorRaController.php
@@ -842,7 +842,7 @@ class MotorRaController extends Controller
                 $reserva->cliente->email,
                 $arr,
                 "correos.comprobante_reserva_motor",
-                "",
+                "", // correo de la propiedad
                 "",
                 1,
                 "",
