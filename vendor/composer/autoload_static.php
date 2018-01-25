a670ed59d5f8a72554e0405a78e5970af99da50d
dheresmann <daniel@holajarvis.com>
2018-01-25 10:32:26 -0300

    eliminar imagen s3

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,22 +4,22 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3
+class ComposerStaticInit2f173d73b6005e1196c003815873ac64
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '1d1b89d124cc9cb8219922c9d5569199' => __DIR__ . '/..' . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
         '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
-        'ad155f8f1cf0d418fe49e248db8c661b' => __DIR__ . '/..' . '/react/promise/src/functions_include.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
-        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
+        'ad155f8f1cf0d418fe49e248db8c661b' => __DIR__ . '/..' . '/react/promise/src/functions_include.php',
         'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
+        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
         'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
@@ -5425,10 +5425,10 @@ class ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitc55480d7b8cd8c3caf0d5c3fc32297f3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2f173d73b6005e1196c003815873ac64::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2f173d73b6005e1196c003815873ac64::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit2f173d73b6005e1196c003815873ac64::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit2f173d73b6005e1196c003815873ac64::$classMap;
 
         }, null, ClassLoader::class);
     }
