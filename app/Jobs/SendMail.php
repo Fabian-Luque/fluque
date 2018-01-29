20c3a3452845cef49503089da6b95b5926b7e6b4
dheresmann <daniel@holajarvis.com>
2018-01-29 16:59:44 -0300

    cambio de estilo comp

--- app/Jobs/SendMail.php
+++ app/Jobs/SendMail.php
@@ -90,7 +90,7 @@ class SendMail extends Job implements ShouldQueue {
                 ];
             } elseif ($array['arr']['comp'] == 1) {
                 $data_correo = [
-                    'reservaspdf'  => $array['arr']['reservaspdf'],
+                    'reservaspdf'  => $array['arr']['reservas_pdf'],
                     'array'        => $array,
                     'iva'          => $array['arr']['iva'],
                     'subtotal'     => $array['arr']['neto'],
