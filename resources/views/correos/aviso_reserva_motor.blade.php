19e6e36095ff39c09e93fcd8ac5da2249830c1f2
daniel <daniel@holajarvis.com>
2018-02-12 18:51:35 -0300

    comp reserva motor cambio diseño

--- resources/views/correos/aviso_reserva_motor.blade.php
+++ resources/views/correos/aviso_reserva_motor.blade.php
@@ -197,28 +197,206 @@ a[x-apple-data-detectors=true] {
       }
     }
   </style>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>
+    <!--[if gte mso 9]><xml>
+     <o:OfficeDocumentSettings>
+      <o:AllowPNG/>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+     </o:OfficeDocumentSettings>
+    </xml><![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
+    <title></title>
+    
+    
+    <style type="text/css" id="media-query">
+      body {
+  margin: 0;
+  padding: 0; }
 
-<!-- [if gte mso 9]><xml>
-     <o:OfficeDocumentSettings>
-      <o:AllowPNG/>
-      <o:PixelsPerInch>96</o:PixelsPerInch>
-     </o:OfficeDocumentSettings>
-    </xml><![endif]-->
-<p></p>
-<!-- [if !mso]><!--><!--<![endif]-->
-<p></p>
-<!-- [if gte mso 9]><xml>
-     <o:OfficeDocumentSettings>
-      <o:AllowPNG/>
-      <o:PixelsPerInch>96</o:PixelsPerInch>
-     </o:OfficeDocumentSettings>
-    </xml><![endif]-->
-<p>&nbsp;</p>
-<!-- [if !mso]><!--><!--<![endif]-->
-<p>&nbsp;</p>
-<!-- [if IE]><div class="ie-browser"><![endif]-->
-<p>&nbsp;</p>
-<!-- [if mso]><div class="mso-container"><![endif]-->
+table, tr, td {
+  vertical-align: top;
+  border-collapse: collapse; }
+
+.ie-browser table, .mso-container table {
+  table-layout: fixed; }
+
+* {
+  line-height: inherit; }
+
+a[x-apple-data-detectors=true] {
+  color: inherit !important;
+  text-decoration: none !important; }
+
+[owa] .img-container div, [owa] .img-container button {
+  display: block !important; }
+
+[owa] .fullwidth button {
+  width: 100% !important; }
+
+[owa] .block-grid .col {
+  display: table-cell;
+  float: none !important;
+  vertical-align: top; }
+
+.ie-browser .num12, .ie-browser .block-grid, [owa] .num12, [owa] .block-grid {
+  width: 625px !important; }
+
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+  line-height: 100%; }
+
+.ie-browser .mixed-two-up .num4, [owa] .mixed-two-up .num4 {
+  width: 208px !important; }
+
+.ie-browser .mixed-two-up .num8, [owa] .mixed-two-up .num8 {
+  width: 416px !important; }
+
+.ie-browser .block-grid.two-up .col, [owa] .block-grid.two-up .col {
+  width: 312px !important; }
+
+.ie-browser .block-grid.three-up .col, [owa] .block-grid.three-up .col {
+  width: 208px !important; }
+
+.ie-browser .block-grid.four-up .col, [owa] .block-grid.four-up .col {
+  width: 156px !important; }
+
+.ie-browser .block-grid.five-up .col, [owa] .block-grid.five-up .col {
+  width: 125px !important; }
+
+.ie-browser .block-grid.six-up .col, [owa] .block-grid.six-up .col {
+  width: 104px !important; }
+
+.ie-browser .block-grid.seven-up .col, [owa] .block-grid.seven-up .col {
+  width: 89px !important; }
+
+.ie-browser .block-grid.eight-up .col, [owa] .block-grid.eight-up .col {
+  width: 78px !important; }
+
+.ie-browser .block-grid.nine-up .col, [owa] .block-grid.nine-up .col {
+  width: 69px !important; }
+
+.ie-browser .block-grid.ten-up .col, [owa] .block-grid.ten-up .col {
+  width: 62px !important; }
+
+.ie-browser .block-grid.eleven-up .col, [owa] .block-grid.eleven-up .col {
+  width: 56px !important; }
+
+.ie-browser .block-grid.twelve-up .col, [owa] .block-grid.twelve-up .col {
+  width: 52px !important; }
+
+@media only screen and (min-width: 645px) {
+  .block-grid {
+    width: 625px !important; }
+  .block-grid .col {
+    vertical-align: top; }
+    .block-grid .col.num12 {
+      width: 625px !important; }
+  .block-grid.mixed-two-up .col.num4 {
+    width: 208px !important; }
+  .block-grid.mixed-two-up .col.num8 {
+    width: 416px !important; }
+  .block-grid.two-up .col {
+    width: 312px !important; }
+  .block-grid.three-up .col {
+    width: 208px !important; }
+  .block-grid.four-up .col {
+    width: 156px !important; }
+  .block-grid.five-up .col {
+    width: 125px !important; }
+  .block-grid.six-up .col {
+    width: 104px !important; }
+  .block-grid.seven-up .col {
+    width: 89px !important; }
+  .block-grid.eight-up .col {
+    width: 78px !important; }
+  .block-grid.nine-up .col {
+    width: 69px !important; }
+  .block-grid.ten-up .col {
+    width: 62px !important; }
+  .block-grid.eleven-up .col {
+    width: 56px !important; }
+  .block-grid.twelve-up .col {
+    width: 52px !important; } }
+
+@media (max-width: 645px) {
+  .block-grid, .col {
+    min-width: 320px !important;
+    max-width: 100% !important;
+    display: block !important; }
+  .block-grid {
+    width: calc(100% - 40px) !important; }
+  .col {
+    width: 100% !important; }
+    .col > div {
+      margin: 0 auto; }
+  img.fullwidth, img.fullwidthOnMobile {
+    max-width: 100% !important; }
+  .no-stack .col {
+    min-width: 0 !important;
+    display: table-cell !important; }
+  .no-stack.two-up .col {
+    width: 50% !important; }
+  .no-stack.mixed-two-up .col.num4 {
+    width: 33% !important; }
+  .no-stack.mixed-two-up .col.num8 {
+    width: 66% !important; }
+  .no-stack.three-up .col.num4 {
+    width: 33% !important; }
+  .no-stack.four-up .col.num3 {
+    width: 25% !important; } }
+
+    </style>
+</head>
+<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #161821">
+  <style type="text/css" id="media-query-bodytag">
+    @media (max-width: 520px) {
+      .block-grid {
+        min-width: 320px!important;
+        max-width: 100%!important;
+        width: 100%!important;
+        display: block!important;
+      }
+
+      .col {
+        min-width: 320px!important;
+        max-width: 100%!important;
+        width: 100%!important;
+        display: block!important;
+      }
+
+        .col > div {
+          margin: 0 auto;
+        }
+
+      img.fullwidth {
+        max-width: 100%!important;
+      }
+      img.fullwidthOnMobile {
+        max-width: 100%!important;
+      }
+      .no-stack .col {
+        min-width: 0!important;
+        display: table-cell!important;
+      }
+      .no-stack.two-up .col {
+        width: 50%!important;
+      }
+      .no-stack.mixed-two-up .col.num4 {
+        width: 33%!important;
+      }
+      .no-stack.mixed-two-up .col.num8 {
+        width: 66%!important;
+      }
+      .no-stack.three-up .col.num4 {
+        width: 33%!important
+      }
+      .no-stack.four-up .col.num3 {
+        width: 25%!important
+      }
+    }
+  </style>
+
 <table class="nl-container" style="border-collapse: collapse; table-layout: fixed; border-spacing: 0; mso-table-lspace: 0pt; mso-table-rspace: 0pt; vertical-align: top; min-width: 320px; margin: 0 auto; background-color: #161821; width: 100%;" cellspacing="0" cellpadding="0">
 <tbody>
 <tr style="vertical-align: top;">
@@ -310,7 +488,7 @@ function quitar_acentos($cadena){
 </tr>
 </tbody>
 </table>
-<!-- [if (mso)|(IE)]></div><![endif]-->
-
+</body>
+</html>
 </body>
 </html>
