60ab8bb3f40c0001f984aeb02e91b3036b479fe1
Allen <allenrcs@gmail.com>
2018-02-13 14:06:52 -0300

    comenta token

--- app/Reserva.php
+++ app/Reserva.php
@@ -88,20 +88,20 @@ class Reserva extends Model {
 				->withTimestamps();
 	}
 
-	// public function getCreatedAtAttribute($value)
- //    {
- //        $token = JWTAuth::getToken();
- //        if ($token) {
- //    	$user            = JWTAuth::parseToken()->toUser();
- //        $propiedad       = $user->propiedad[0];
- //        $zona_horaria_id = $propiedad->zona_horaria_id;
- //        $zona_horaria    = ZonaHoraria::where('id', $zona_horaria_id)->first();
- //        $pais            = $zona_horaria['nombre'];
- //        return Carbon::parse($value)->timezone($pais)->format('Y-m-d H:i:s');
- //        } 
+	public function getCreatedAtAttribute($value)
+    {
+        $token = JWTAuth::getToken();
+        if ($token) {
+    	$user            = JWTAuth::parseToken()->toUser();
+        $propiedad       = $user->propiedad[0];
+        $zona_horaria_id = $propiedad->zona_horaria_id;
+        $zona_horaria    = ZonaHoraria::where('id', $zona_horaria_id)->first();
+        $pais            = $zona_horaria['nombre'];
+        return Carbon::parse($value)->timezone($pais)->format('Y-m-d H:i:s');
+        } 
     	
 
- //    }   
+    }   
 
 
 
