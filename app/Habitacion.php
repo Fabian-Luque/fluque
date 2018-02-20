60da7893933ad748f8147bbb0eecd247a039b1c7
Allen <allenrcs@gmail.com>
2018-02-20 18:12:23 -0300

    elimina piso de habitaciones

--- app/Habitacion.php
+++ app/Habitacion.php
@@ -12,7 +12,7 @@ class Habitacion extends Model
 
 	protected $table = 'habitaciones';
 
-	protected $fillable = ['nombre','piso','estado_habitacion_id','tipo_habitacion_id'];
+	protected $fillable = ['nombre','estado_habitacion_id','tipo_habitacion_id'];
 
 	public function propiedad(){
 
