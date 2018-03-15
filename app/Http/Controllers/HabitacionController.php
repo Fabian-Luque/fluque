c5d7550b480c0aaabfa363d34d480dc0e97b2b2a
Allen <allenrcs@gmail.com>
2018-03-15 18:16:39 -0300

    fix editar tipo de habitaion de hab

--- app/Http/Controllers/HabitacionController.php
+++ app/Http/Controllers/HabitacionController.php
@@ -378,12 +378,15 @@ class HabitacionController extends Controller
         } else {
 
             $habitacion       = Habitacion::findOrFail($id);
-            $tipo_habitacion  = TipoHabitacion::where('id', $habitacion->tipo_habitacion_id)->first();
-            $cantidad         = $tipo_habitacion->cantidad;
-            $disponible_venta = $tipo_habitacion->disponible_venta;
-            $tipo_habitacion->update(array('cantidad' => $cantidad - 1));
-            if ($cantidad - 1 < $disponible_venta) {
-                $tipo_habitacion->update(array('disponible_venta' => $tipo_habitacion->disponible_venta - 1));
+            $tipo_habitacion_id = $habitacion->tipo_habitacion_id;
+            if (!is_null($tipo_habitacion_id)) {
+                $tipo_habitacion  = TipoHabitacion::where('id', $habitacion->tipo_habitacion_id)->first();
+                $cantidad         = $tipo_habitacion->cantidad;
+                $disponible_venta = $tipo_habitacion->disponible_venta;
+                $tipo_habitacion->update(array('cantidad' => $cantidad - 1));
+                if ($cantidad - 1 < $disponible_venta) {
+                    $tipo_habitacion->update(array('disponible_venta' => $tipo_habitacion->disponible_venta - 1));
+                }
             }
             $habitacion->update($request->all());
             $habitacion->touch();
