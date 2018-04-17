05b9eb80176f5735c0a3bfa5b3e59e7fe8cb2ce9
dheresmannb <dheresmann2012@alu.uct.cl>
2018-04-17 11:16:46 -0300

    modificacion evento + datos de pago

--- app/Events/PagoFacilEvent.php
+++ app/Events/PagoFacilEvent.php
@@ -8,13 +8,11 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class PagoFacilEvent extends Event {
     use SerializesModels;
-    public $data;
     public $evento;
     public $pasarela;
     public $data_pago;
 
-    public function __construct($propiedad_id, $pasa, $data_pago) {
-        $this->data      = $propiedad_id;
+    public function __construct($pasa, $data_pago) {
         $this->evento    = "pago-online";
         $this->pasarela  = $pasa;
         $this->data_pago = $data_pago;
